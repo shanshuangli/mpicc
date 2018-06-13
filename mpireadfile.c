93cbfd02e56b640dff10db497e90a00d51b3fb44
shan <shanshuangli1@gmail.com>
2018-06-13 20:36:41 +0800

    update

--- mpireadfile.c
+++ mpireadfile.c
@@ -80,7 +80,4 @@ int main(int argc, char *argv[])
     MPI_Finalize();
     return 0;
 }
-void judge(Point p[],int n){
-	
-}
 
--- turn.c
+++ turn.c
@@ -1,4 +1,4 @@
-//将数组文本文件转换为二进制文件。要求文本文件的内容：行数 列数 数组元素
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,29 +12,31 @@ int main()
     FILE *fp,*fq;
     fp=fopen("traj_gps.txt","r");
     fq=fopen("traj_gps","wb");
-    int i=1;
+   
+while(!feof(fp)){
+ int i=1;
+    fscanf(fp,"%d",&t);
+    fwrite(&t,sizeof(int),1,fq);
 
     fscanf(fp,"%d",&t);
-fwrite(&t,sizeof(int),1,fq);
-printf("%d\n",t);
-    fscanf(fp,"%d",&t);
-printf("%d\n",t);
-fwrite(&t,sizeof(int),1,fq);
+
+    fwrite(&t,sizeof(int),1,fq);
     while(i<t+1)
     {
 	fscanf(fp,"%d",&t1);
 	fwrite(&t1,sizeof(int),1,fq);
-printf("%d\n",t1);
+
         fscanf(fp,"%f",&tmp);
-printf("%f\n",tmp);
+ 
 	fwrite(&tmp,sizeof(float),1,fq);
         fscanf(fp,"%f",&tmp);
-	printf("%f\n",tmp);
+	
         fwrite(&tmp,sizeof(float),1,fq);
 	i++;	
-    }    
+    }  
+}  
 
     fclose(fp);
     fclose(fq);
-    fprintf(stdout,"Successfully!\n");
+
 }
